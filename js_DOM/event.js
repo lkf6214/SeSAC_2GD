122ed0ffb4b0fcfddf556c3e49f2bcc92a0f5ac3
lkf6214 <lkf6214@naver.com>
2023-07-07 15:41:28 +0900

    jsdom

--- js_DOM/event.js
+++ js_DOM/event.js
@@ -47,5 +47,94 @@ window.addEventListener("scroll", () => {
   console.log(scrollY);
   if (scrollY > 900) {
     document.querySelector(".pos").style.opacity = "1";
+  } else {
+    document.querySelector(".pos").style.opacity = "0.3";
   }
+});
+
+const btn = document.querySelector("button");
+const input = document.querySelector("input");
+
+// addEventListener는 매개변수 하나만 들어가야함.
+// function() <= 요 안에 들어가는게 매개변수
+// event 말고 다른거 들어가도 됨
+btn.addEventListener("click", function (event) {
+  console.log(event);
+});
+
+// click 클릭하면 click에 대한 정보가 event  객체에 담겨있음
+
+// btn.addEventListener("keybord", function (event) {
+//   console.log(event.key);
+//   console.log(event.code);
+// });
+
+input.addEventListener("keydown", function (event) {
+  console.log(event.key);
+  console.log(event.code);
+
+  // 방향키 위 아래 왼쪽 오른쪽을 누르면
+  // 각각 누를 때마다 "up", "down", "left", "right"
+  if (event.key === "ArrowUp") {
+    console.log("up");
+  } else if (event.key === "ArrowDown") {
+    console.log("down");
+  } else if (event.key === "ArrowLeft") {
+    console.log("left");
+  } else if (event.key === "ArrowRight") {
+    console.log("right");
+  } else {
+    console.log("others");
+  }
+});
+// 변경 이벤트
+const chgInput = document.getElementById("change-input");
+chgInput.addEventListener("input", () => {
+  console.log("변경중!!");
+  let div = document.querySelector(".box");
+  div.innerHTML = chgInput.value;
+  /* 
+  input: 현재 input 에 변경사항이 존재하는 순간을 계속 인식
+  실시간으로 바로 반영할 수 있음
+
+  shift, ctrl 등 입력값이 변하지 않는다면 이벤트 발생 x
+  */
+});
+chgInput.addEventListener("change", () => {
+  // 'change' : input 에 변경이 일어나고
+  // 다른 요소를 클릭해서 focus out 시키면 이벤트 발생됨
+  console.log("변경되었어요!!");
+});
+
+// -----------------
+// 폼이 제출되는 이벤트
+const todoForm = document.querySelector("#todo-form");
+const todos = document.querySelector(".todos");
+// console.log(todoForm);
+// console.log(todos);
+todoForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  /// 폼이 실제로 제출되지 않도록 이벤트를 막는 것.
+  // 새로고침을 막기 위한 것
+  const todoInput = document.querySelector('[name = "todo"]');
+  // console.log(todoInput.value);
+  // input 창 내용은 value 임.
+
+  // const todo = todoInput.value;
+  // const li = document.createElement("li");
+  // li.append(todo);
+  // // <li>안녕</li>asdf
+  // todos.append(li);
+
+  // 만약 공백이 있을 때는 추가하고 싶지 않다
+
+  if (todoInput.value.trim() !== "") {
+    const li = document.createElement("li");
+    li.append(todoInput.value);
+    console.log(todoInput);
+    todos.append(li);
+  } else {
+    alert("공백문자는 추가할 수 없습니다.");
+  }
+  todoInput.value = "";
 });

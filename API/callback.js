1502498acb4216c0ecdcc6e8b885819734994ddf
lkf6214 <lkf6214@naver.com>
2023-08-25 14:20:39 +0900

    클래스

---
+++ API/callback.js
@@ -0,0 +1,25 @@
+let
+id = prompt 아이디를 입력하세요
+let
+userId =
+console
+log 로그인 시도
+//
+서버 통신 부분
+setTimeout
+function cb1 ()
+userId
+= allie1
+console
+log 아이디 정보 획득 완료
+},
+2000
+//
+데이터 검사if id === userId )
+console
+log 로그인 성공
+}
+else
+console
+log 로그인 실패
+}
